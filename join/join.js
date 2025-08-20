12c3898f073e451fa509f0d4e3bcf128a4db18da
moonkanghyeon <s25024@gsm.hs.kr>
2025-08-20 15:46:16 +0900

    email 인증 추가

--- join/join.js
+++ join/join.js
@@ -1,5 +1,7 @@
-const buttonColor = document.querySelector(".gender button");
-buttonColor.addEventListener("click", (click) => {
-  buttonColor.style.backgroundColor = "#91bbff";
-  buttonColor.style.color = "#fff";
-});
+// const buttonColor = document.querySelector(".gender button");
+// buttonColor.addEventListener("click", (click) => {
+//   buttonColor.style.backgroundColor = "#91bbff";
+//   buttonColor.style.color = "#fff";
+// });
+
+//아직 미완성
