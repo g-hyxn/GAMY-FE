e6cacc268e937bba522cc38c778e90d6aa9d9656
moonkanghyeon <s25024@gsm.hs.kr>
2025-08-20 14:52:23 +0900

    GAMY-FE 서브모듈 변경사항 커밋

---
+++ join/join.js
@@ -0,0 +1,5 @@
+const buttonColor = document.querySelector(".gender button");
+buttonColor.addEventListener("click", (click) => {
+  buttonColor.style.backgroundColor = "#91bbff";
+  buttonColor.style.color = "#fff";
+});
